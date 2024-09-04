7b0785f6f6ddef2d0ca432e1df1a1f8e873f3a34
unknown <krishnateach716@gmail.com>
2024-09-04 09:01:08 +0530

    data type rules add

--- chapter 1/datatype.py
+++ chapter 1/datatype.py
@@ -3,7 +3,7 @@
 a = 1  #a is an integer
 b = 5.22   #b is float point number
 c = "krishna" #c is string 
-d = False/True  #d is a boolean value number
+d = False  #d is a boolean value number
 e= None #e is none type variable
 
 _samer =45
