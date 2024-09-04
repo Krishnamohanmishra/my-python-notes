d46b02cbf48b41f18ddf57144c51723030c00d72
unknown <krishnateach716@gmail.com>
2024-09-04 08:35:20 +0530

    rules of variable name

--- chapter 1/datatypes.py
+++ chapter 1/datatypes.py
@@ -5,3 +5,10 @@ b = 5.22   #b is float point number
 c = "krishna" #c is string 
 d = False  #d is a boolean value number
 e= None #e is none type variable
+
+_samer =45
+sameer=23
+krishna =100
+qqasddrr=23
+#@sameer =56 is invaild due to @symbol
+#s@meer =56 is invaild due to @symbol
