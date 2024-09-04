930cd0f4b1a84210b554e4d336c3608fed8eb255
unknown <krishnateach716@gmail.com>
2024-09-04 08:54:02 +0530

    data type

---
+++ chapter 1/operater.py
@@ -0,0 +1,21 @@
+# operater in python 
+
+       #Arithmetic Operators------------
+
+a=3
+b=4
+c=a+b
+print(c)
+       #assigment operator---------------
+a=4-3
+print(a)
+
+b=6
+b+=3#increment the value of b by 3 and then assign it to b
+print(b)
+
+
+      #comparison operator----------------
+
+a = 3==3 #it is means 3 equal to 3
+print(a)
