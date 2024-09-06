bb85a19945e481d8009bfac9e43cdb10c64d965c
unknown <krishnateach716@gmail.com>
2024-09-06 08:22:49 +0530

    push

--- chapter 1/operater.py
+++ chapter 1/operater.py
@@ -15,7 +15,23 @@ b+=3#increment the value of b by 3 and then assign it to b
 print(b)
 
 
+c=3
+c*=7
+print(c)
+
+
       #comparison operator----------------
 
 a = 3==3 #it is means 3 equal to 3
 print(a)
+
+a= 5>=3
+print(a)
+
+a= 3454<=556
+print(a)
+
+a= 5!=5 
+print(a)
+
+         #logical operater----------
