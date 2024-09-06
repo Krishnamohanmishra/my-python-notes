ef12b719062d84cf6acbbfec53fae5589a4c3798
unknown <krishnateach716@gmail.com>
2024-09-06 09:49:17 +0530

    problem 1 sol

---
+++ chapter 1/mixproblem solving.py
@@ -0,0 +1,82 @@
+# What are the different types of operators available in Python, and provide examples of each?
+'''exm   arthmetic operator---- ---- - ----- ----- ----'''
+
+a=4
+b=4
+print(a+b)
+
+a= 5
+b=7
+print(a-b)
+
+
+a=4
+b=8
+print(a*b)
+
+a=8
+b=4
+print(a/b)
+
+a=8
+b=4
+print(a%b)
+
+''' assigment operator ---------- -------- ----- - - - - - - - - - '''
+a=3
+a+=5
+b=4
+print(a)
+print(a+b)
+
+a=4
+a-=5
+print(a)
+
+a=9
+a*=7
+print(a)
+
+a=4
+a/=7
+print(a)
+
+a=4
+a%=7
+print(a)
+
+
+'''comprision operator------- --------- ----- ----'''
+
+a=3==4
+print(a)
+
+
+a=6<9 
+print(a)
+
+
+a=5<=4
+print(a)
+
+
+a=5>9
+print(a)
+
+
+a=4>=4
+print(a)
+
+a=8!=3
+print(a)
+
+
+
+'''logical operator ------- ----- - - - - - -- --  -  - - ----------'''
+
+
+print("False or true is" ,False or True)
+
+print("False and true is" ,False and True)
+
+print(not(True))
